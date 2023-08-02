375e09f6e41532714dbba5c1bb7ca99550b28f36
geummiri <127584256+geummiri@users.noreply.github.com>
2023-08-02 14:06:17 +0900

    <feat><Servlet_04_ServletLifecycle>

---
+++ 04_ServletLifecycle/src/servlet/life/LifecycleServlet1.java
@@ -0,0 +1,47 @@
+package servlet.life;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LifecycleServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	public int count=0;
+
+	//클래스명과 이름이 동일한 메서드 생성은 => 생성자 생성
+    public LifecycleServlet1() {
+    	System.out.println("1. LifecycleServlet 생성자..");
+    }
+    
+    //서블릿이 처음으로 생성되거나 컨테이너에 의해 초기화될 때 호출
+    //계속 호출되면 안될때 init이 사용됨
+    //예를 들어 유튜브으 목록 같은 경우
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("2. init...호출..");
+	}
+	//종료될 때 호출
+	public void destroy() {
+		System.out.println("4. destroy...호출..");
+	}
+
+	//비지니스로직이 구현되는 곳
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("3. service...호출..");
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<h2>LifeCycle CallBack Method...</h2>");
+		out.println("<p>Count :: " + ++count + "</p>");
+		
+		out.close();
+		
+	}
+
+}
