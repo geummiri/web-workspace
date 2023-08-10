edf814d55b09d53930e6a0998c970be0c112e8d6
geummiri <127584256+geummiri@users.noreply.github.com>
2023-08-10 17:35:44 +0900

    <feat><17_FactoryPattern>

---
+++ 17_FactoryPattern/src/servlet/controller/ControllerFactory.java
@@ -0,0 +1,43 @@
+package servlet.controller;
+
+import servlet.controller.component.FindController;
+
+public class ControllerFactory {
+
+	
+private static ControllerFactory factory = new ControllerFactory();
+	
+	private ControllerFactory() {
+		//다른곳에서 생성 못하게 private으로 작성
+	}
+	
+	public static ControllerFactory getInstance() { 
+		//생성자 대신 메서드를 다른곳에서 호출 가능하게 public으로 작성
+		return factory;
+	}
+	
+	
+	//createController: 컨트롤러를 생성하는 기능
+//	public Controller createController(String command) {
+//		Controller controller = null;
+//		
+//		if(command.equals("find")) {
+//			controller = new FindController();
+//		}
+//		
+//		return controller;
+//
+//	}
+	
+	
+	public Controller createController(String command) {
+		
+		Controller controller = null;
+		 
+		if(command.equals("find")) {
+			controller = new FindController();
+		}
+		
+		return controller;
+	}
+}
