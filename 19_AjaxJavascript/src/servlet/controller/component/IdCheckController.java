0a637b21daa1f48dd9c7d8c2a69f2871d9eba5a1
geummiri <127584256+geummiri@users.noreply.github.com>
2023-08-21 17:51:19 +0900

    <update>

--- 19_AjaxJavascript/src/servlet/controller/component/IdCheckController.java
+++ 19_AjaxJavascript/src/servlet/controller/component/IdCheckController.java
@@ -11,36 +11,23 @@ import servlet.model.vo.MemberVO;
 
 public class IdCheckController implements Controller{
 
+
 	@Override
 	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String id = request.getParameter("id");
 		MemberVO vo = MemberDAO.getInstance().findByIdMember(id);
 		
 		boolean flag = false;
-		
-		//아이디가 있는지 확인 후 true값으로 반환하기
 		if(vo!=null) {
 			flag = true;
 		}
 		
-		
-		//비동기방식
+		// 비동기방식
 		PrintWriter out = response.getWriter();
 		out.print(flag);
 		
-		
 		return null;
-		
-		
-//		if(vo!=null) {
-//		out.println("ID 사용 불가");
-//	} else {
-//		out.println("ID 사용 가능");
-//	}
-//	
-//	return new ModelAndView();
-		
-		
 	}
+		
 
 }
